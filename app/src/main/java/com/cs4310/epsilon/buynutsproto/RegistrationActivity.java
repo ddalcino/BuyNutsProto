136790ec55bd36327b3542bd2921aa11761580ed
kylechrissaechao <ksaechao52@horizon.csueastbay.edu>
2015-11-19 10:52:15 -0800

    Registration Activity now includes fields for email, phone number and name.

--- app/src/main/java/com/cs4310/epsilon/buynutsproto/RegistrationActivity.java
+++ app/src/main/java/com/cs4310/epsilon/buynutsproto/RegistrationActivity.java
@@ -23,13 +23,28 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void onClickRegisterButton(View view) {
-        //et2Username holds user input for username on Registration Activity Screen
-         EditText et2Username = (EditText) findViewById(R.id.et2Username);
-        //et2Password holds user input for password on Registration Activity Screen
-         EditText et2Password = (EditText) findViewById(R.id.et2Password);
+        try {
+            //et2Username holds user input for Username on Registration Activity Screen
+            EditText et2Username = (EditText) findViewById(R.id.et2Username);
 
-        //Toast displays user inputted "username" and "password" just making sure register button is responsive
-        Toast.makeText(RegistrationActivity.this, "username: " + et2Username.getText().toString() + "\n password: " + et2Password.getText().toString() , Toast.LENGTH_LONG).show();
+            //et2Password holds user input for Password on Registration Activity Screen
+            EditText et2Password = (EditText) findViewById(R.id.et2Password);
 
+            //et2Email holds user input for Email on Registration Activity Screen
+            EditText et2Email = (EditText) findViewById(R.id.et2Email);
+
+            //et2Name holds user input for Name on Registration Activity Screen
+            EditText et2Name = (EditText) findViewById(R.id.et2Name);
+
+            //et2Phone holds user input for Phone on Registration Activity Screen
+            EditText et2Phone = (EditText) findViewById(R.id.et2Phone);
+
+            //Toast displays user inputted "username" and "password" just making sure register button is responsive
+            Toast.makeText(RegistrationActivity.this, "name: " + et2Name.getText().toString() + "\nemail: " + et2Email.getText().toString() + "\nphone: " + et2Phone.getText().toString() + "\nusername: " + et2Username.getText().toString() + "\npassword: " + et2Password.getText().toString(), Toast.LENGTH_LONG).show();
+        }
+        catch(Exception invalid)
+        {
+            invalid.getMessage();
+        }
     }
 }
