937c9fb35d0aa2859f4611c3c754ffe4c33c8c43
kylechrissaechao <ksaechao52@horizon.csueastbay.edu>
2015-11-19 21:05:32 -0800

    Added confirmation field to registration activity and displays whether password and confirm input matches.

--- app/src/main/java/com/cs4310/epsilon/buynutsproto/RegistrationActivity.java
+++ app/src/main/java/com/cs4310/epsilon/buynutsproto/RegistrationActivity.java
@@ -39,8 +39,16 @@ public class RegistrationActivity extends AppCompatActivity {
             //et2Phone holds user input for Phone on Registration Activity Screen
             EditText et2Phone = (EditText) findViewById(R.id.et2Phone);
 
+            //et2Phone holds user input for Phone on Registration Activity Screen
+            EditText et2Confirm = (EditText) findViewById(R.id.et2Confirm);
+
+            //display whether or not password input and confirm input matches
+            if(!et2Confirm.getText().toString().equals(et2Password.getText().toString()))
+            {
+                Toast.makeText(RegistrationActivity.this, "PASSWORD: " + et2Password.getText().toString() + " doesn't match CONFIRM: " + et2Confirm.getText().toString(), Toast.LENGTH_LONG).show();
+            }
             //Toast displays user inputted "username" and "password" just making sure register button is responsive
-            Toast.makeText(RegistrationActivity.this, "name: " + et2Name.getText().toString() + "\nemail: " + et2Email.getText().toString() + "\nphone: " + et2Phone.getText().toString() + "\nusername: " + et2Username.getText().toString() + "\npassword: " + et2Password.getText().toString(), Toast.LENGTH_LONG).show();
+            Toast.makeText(RegistrationActivity.this, "name: " + et2Name.getText().toString() + "\nemail: " + et2Email.getText().toString() + "\nphone: " + et2Phone.getText().toString() + "\nusername: " + et2Username.getText().toString() + "\npassword: " + et2Password.getText().toString() + "\nconfirm: " + et2Confirm.getText().toString(), Toast.LENGTH_LONG).show();
         }
         catch(Exception invalid)
         {
